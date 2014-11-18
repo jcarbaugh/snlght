29af68acd6c2f49dde5c8561eadf77b56e5aedba
Jeremy Carbaugh <jcarbaugh@gmail.com>
2014-11-18 12:33:47 -0500

    load from stdin

--- loader.py
+++ loader.py
@@ -2,31 +2,30 @@ from web import mongo, shorten
 import collections
 import datetime
 import json
+import sys
 
-# seen = set()
 
-# with open('data/results-2014-11-12-AM.json') as infile:
-# 	data = json.load(infile)
-# 	links = data['data']['link_history']
+seen = set()
 
-# data = collections.defaultdict(list)
+data = json.load(sys.stdin)
+links = data['data']['link_history']
 
-# for index, link in enumerate(links):
+for index, link in enumerate(links):
 
-# 	slug = link['link'].rsplit('/', 1)[-1]
+	slug = link['link'].rsplit('/', 1)[-1]
 
-# 	if slug not in seen:
+	if slug not in seen:
 
-# 		doc = shorten(link['long_url'], slug=slug, save=False)
+		doc = shorten(link['long_url'], slug=slug, save=False)
 
-# 		doc['title'] = link['title']
-# 		doc['created_at'] = datetime.datetime.utcfromtimestamp(link['created_at'])
-# 		doc['created_by'] = 'snlght'
-# 		doc['archived'] = link['archived']
-# 		doc['private'] = link['private']
+		doc['title'] = link['title']
+		doc['created_at'] = datetime.datetime.utcfromtimestamp(link['created_at'])
+		doc['created_by'] = 'snlght'
+		doc['archived'] = link['archived']
+		doc['private'] = link['private']
 		
-# 		mongo.links.save(doc)
+		mongo.links.save(doc)
 
-# 		print index, slug
+		print index, slug
 
-# 		seen.add(slug)
+		seen.add(slug)
